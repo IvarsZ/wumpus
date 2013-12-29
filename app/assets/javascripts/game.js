d238d33dc6ba4a8507386bc3c071d137d488c90e
IvarsZ <IZubkans@gmail.com>
2013-12-29 17:38:45 +0200

    Setup

---
+++ app/assets/javascripts/game.js
@@ -0,0 +1,10 @@
+Game = {
+
+  // Initialize and start the game.
+  start: function() {
+
+    // Start crafty and set a background color so that we can see it's working.
+    Crafty.init(400, 400);
+    Crafty.background('rgb(200, 250, 250)');
+  }
+}
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wumpus::Application.config.secret_key_base = 'd67e0e26ef5c5d94d5cb7da0a44105ccc3da2b5518081361a2324fc74902ede3325528f3d91dc54b4e97e7ae4425312298d194c493eabc7b75b1adcb558c60f6'
